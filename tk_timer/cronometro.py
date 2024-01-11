7643e67fcbbcf2fa2c202984dfaa8c032cdad42e
Fabricio <fabriciocolombo0096@gmail.com>
2024-01-11 11:41:01 -0300

    11/01
    tk

--- tk_timer/cronometro.py
+++ tk_timer/cronometro.py
@@ -1,4 +1,5 @@
 import time
+import tkinter as tk
 
 def cronometro():
     for num in range(10, 0, -1):
@@ -6,4 +7,21 @@ def cronometro():
         print(num, end=' \r')
     print('fim')
     
-cronometro()
+#cronometro()
+
+
+janela = tk.Tk()
+
+label = tk.Label(janela, text='TEXTO DA JANELA', width=50, height=2, foreground='Black', relief='groove',)
+label.pack()
+
+botao1 = tk.Button(janela, text='BOTÃO 1', bg='Black', height=1, font=4, foreground='Red', relief='sunken')
+botao1.pack()
+
+botao2 = tk.Button(janela, text='BOTÃO 2', bg='Black', height=1, font=4, foreground='Red', relief='ridge')
+botao2.pack()
+
+botao3 = tk.Entry(janela, textvariable='HAHA', insertbackground='Green')
+botao3.pack()
+
+janela.mainloop() 
