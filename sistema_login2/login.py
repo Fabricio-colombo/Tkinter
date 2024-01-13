20ae14b4798981400928c9cce6b41e509e3b5ef7
Fabricio <fabriciocolombo0096@gmail.com>
2024-01-13 08:14:54 -0300

    x

---
+++ sistema_login2/login.py
@@ -0,0 +1,103 @@
+import tkinter as tk
+from tkinter import messagebox
+import pygame
+import sys
+
+# Função para verificar o login e senha
+def verificar_login():
+    login = entry_login.get()
+    senha = entry_senha.get()
+
+    if login == "fabriciodoidao" and senha == "fabriciomaluco":
+        messagebox.showinfo("Sucesso", "Parabéns, você entrou no sistema!")
+        # Fechar a janela de login
+        window.destroy()
+        # Abrir a nova janela com o jogo da cobrinha
+        abrir_janela_jogo()
+    else:
+        messagebox.showerror("Erro", "Login ou senha incorretos. Tente novamente.")
+
+# Função para abrir a janela do jogo da cobrinha
+def abrir_janela_jogo():
+    # Criar a janela do jogo
+    jogo_window = tk.Tk()
+    jogo_window.title("Jogo da Cobrinha")
+    jogo_window.geometry("400x400")
+
+    # Configurar a cor de fundo da janela para preto
+    jogo_window.configure(bg="black")
+
+    # Inicialização do Pygame
+    pygame.init()
+
+    # Configurações da tela
+    width, height = 400, 400
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Jogo da Cobrinha")
+
+    # Cores
+    golden_color = (255, 215, 0)  # Cor dourada em formato RGB
+
+    # Classe Snake (Cobra)
+    class Snake:
+        def __init__(self):
+            self.size = 13  # Tamanho da cobrinha
+            self.x = width // 2
+            self.y = height // 2
+            self.color = golden_color
+            self.direction = "right"
+
+        def draw(self):
+            pygame.draw.rect(screen, self.color, (self.x, self.y, self.size, self.size))
+
+    # Instância da cobrinha
+    snake = Snake()
+
+    # Loop principal
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Lógica do jogo aqui
+        screen.fill((0, 0, 0))  # Preenche o fundo com preto
+        snake.draw()  # Desenha a cobrinha na tela
+
+        pygame.display.flip()
+
+    # Iniciar o loop principal da aplicação do jogo
+    jogo_window.mainloop()
+
+# Criar a janela principal
+window = tk.Tk()
+window.title("Sistema de Login")
+window.geometry("300x200")
+
+# Configurar a cor de fundo da janela para verde
+window.configure(bg="#D8CEC6")
+
+# Criar os widgets da janela
+label_title = tk.Label(window, text="Sistema de Login", font=("Arial", 16, "bold"), fg="#cf8f2a")
+label_title.pack(pady=20)
+
+label_login = tk.Label(window, text="Login:", bg="#c6d0d8")
+label_login.pack()
+
+entry_login = tk.Entry(window, bg="#c6d0d8")
+entry_login.pack()
+
+label_senha = tk.Label(window, text="Senha:", bg="#c6d0d8")
+label_senha.pack()
+
+entry_senha = tk.Entry(window, show="*", bg="#c6d0d8")
+entry_senha.pack()
+
+button_entrar = tk.Button(window, text="Entrar", command=verificar_login)
+button_entrar.pack()
+
+# Centralizar a janela
+window.eval('tk::PlaceWindow %s center' % window.winfo_toplevel())
+
+# Iniciar o loop principal da aplicação
+window.mainloop()
--- tk_timer/cronometro.py
+++ tk_timer/cronometro.py
@@ -1,27 +1,62 @@
-import time
 import tkinter as tk
+import time
 
-def cronometro():
-    for num in range(10, 0, -1):
-        time.sleep(1)
-        print(num, end=' \r')
-    print('fim')
+class Cronometro:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Meu_Cronometro")
+        self.root.geometry("500x400")
+
+        self.root.configure(bg="navy")
+
+        self.frame = tk.Frame(self.root, bg="navy")
+        self.frame.pack(pady=10)
+
+        self.titulo = tk.Label(self.frame, text="Meu_Cronometro", font=("Arial", 20), bg="navy", fg="white")
+        self.titulo.pack()
+
+        self.buttons_frame = tk.Frame(self.root, bg="navy")
+        self.buttons_frame.pack(pady=10)
+
+        self.iniciar = tk.Button(self.buttons_frame, text="Iniciar", command=self.iniciar_cronometro, bg="green", fg="white", font=("Arial", 16))
+        self.iniciar.pack(side=tk.LEFT, padx=5)
+
+        self.parar = tk.Button(self.buttons_frame, text="Parar", command=self.parar_cronometro, state=tk.DISABLED, bg="red", fg="white", font=("Arial", 16))
+        self.parar.pack(side=tk.LEFT, padx=5)
+
+        self.label = tk.Label(self.root, text="00:00:00", font=("Arial", 24), bg="navy", fg="white")
+        self.label.pack(pady=20)
+
+        self.tempo_inicial = None
+        self.tempo_parado = 0
+        self.executando = False
     
-#cronometro()
-
-
-janela = tk.Tk()
-
-label = tk.Label(janela, text='TEXTO DA JANELA', width=50, height=2, foreground='Black', relief='groove',)
-label.pack()
-
-botao1 = tk.Button(janela, text='BOTÃO 1', bg='Black', height=1, font=4, foreground='Red', relief='sunken')
-botao1.pack()
-
-botao2 = tk.Button(janela, text='BOTÃO 2', bg='Black', height=1, font=4, foreground='Red', relief='ridge')
-botao2.pack()
-
-botao3 = tk.Entry(janela, textvariable='HAHA', insertbackground='Green')
-botao3.pack()
-
-janela.mainloop() 
+    def iniciar_cronometro(self):
+        if not self.executando:
+            self.tempo_inicial = time.time() - self.tempo_parado
+            self.executando = True
+            self.atualizar_cronometro()
+            self.iniciar.config(state=tk.DISABLED)
+            self.parar.config(state=tk.NORMAL)
+            
+    def parar_cronometro(self):
+        if self.executando:
+            self.root.after_cancel(self.atualizar_cronometro)
+            self.tempo_parado = time.time() - self.tempo_inicial
+            self.executando = False
+            self.iniciar.config(state=tk.NORMAL)
+            self.parar.config(state=tk.DISABLED)
+            
+    def atualizar_cronometro(self):
+        if self.executando:
+            tempo_atual = time.time() - self.tempo_inicial
+            horas = int(tempo_atual / 3600)
+            minutos = int((tempo_atual % 3600) / 60)
+            segundos = int(tempo_atual % 60)
+            tempo_formatado = f"{horas:02d}:{minutos:02d}:{segundos:02d}"
+            self.label.config(text=tempo_formatado)
+            self.root.after(1000, self.atualizar_cronometro)
+            
+root = tk.Tk()
+cronometro = Cronometro(root)
+root.mainloop()
